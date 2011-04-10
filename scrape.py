f8e990775d028332075dfd5c62e17f9099758ee0
unknown <Steve@.(none)>
2011-04-10 14:45:55 +0100

    refactored to getNags

--- scrape.py
+++ scrape.py
@@ -1,12 +1,15 @@
 from BeautifulSoup import BeautifulSoup
 from urllib2 import urlopen
 
-response = urlopen("http://www.racingpost.com/horses2/cards/card.sd?race_id=526624&r_date=2011-04-10")
-html = response.read()
+url="http://www.racingpost.com/horses2/cards/card.sd?race_id=526624&r_date=2011-04-10"
 
-soup = BeautifulSoup(html)
+def getNags(url):
+    html = urlopen(url).read()
+    soup = BeautifulSoup(html)
+    for horsesoup in soup.findAll("table", {"class":'cardSt'}):
+        for possiblehorse in horsesoup.findAll("b"):
+            if len(possiblehorse.text) > 5:
+                print possiblehorse.text.strip() 
 
-for horsesoup in soup.findAll("table", {"class":'cardSt'}):
-    for possiblehorse in horsesoup.findAll("b"):
-        if len(possiblehorse.text) > 5:
-            print possiblehorse.text.strip() 
+
+getNags(url)
