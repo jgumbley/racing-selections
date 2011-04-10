ff5be24ce56168abeee6902e8af05e7ebaa337a5
unknown <Steve@.(none)>
2011-04-10 14:41:17 +0100

    nag names from racing post page

--- scrape.py
+++ scrape.py
@@ -4,4 +4,9 @@ from urllib2 import urlopen
 response = urlopen("http://www.racingpost.com/horses2/cards/card.sd?race_id=526624&r_date=2011-04-10")
 html = response.read()
 
-print html
+soup = BeautifulSoup(html)
+
+for horsesoup in soup.findAll("table", {"class":'cardSt'}):
+    for possiblehorse in horsesoup.findAll("b"):
+        if len(possiblehorse.text) > 5:
+            print possiblehorse.text.strip() 
